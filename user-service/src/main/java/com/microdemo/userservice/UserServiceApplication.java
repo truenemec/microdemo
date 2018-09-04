9d60e4650245dc47e82b15219ee10c98064d8259
truenemec <truenemec@mail.ru>
2018-09-04 22:19:12 +0300

    added hystrix

--- user-service/src/main/java/com/microdemo/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/microdemo/userservice/UserServiceApplication.java
@@ -2,8 +2,10 @@ package com.microdemo.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
--- user-service/src/main/java/com/microdemo/userservice/controller/UserController.java
+++ user-service/src/main/java/com/microdemo/userservice/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.microdemo.userservice.controller;
 
 import com.microdemo.userservice.domain.User;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
     @GetMapping
+    @HystrixCommand(fallbackMethod = "getDefault")
     public User get(){
         User user = new User();
         user.setId(1L);
         return user;
+    }
+
+    public User getDefault() {
+        return null;
     }
 }
